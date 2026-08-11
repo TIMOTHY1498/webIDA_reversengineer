987e4e875bd06f2b5775a39d1fc4df4bc129496a
TIMOTHY1498 <timothyb.s@outlook.co.id>
2026-08-11 20:08:09 +0700

    remove duplicate

--- frontend/jsout/index_type.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ResourceEntryBaseType, ResourceEntryT, ResourceEntryTT } from './ResourceEntry.js';
-import type { ResourceEntry } from './ResourceEntry.js';
-export type { ResourceEntry, ResourceEntryBaseType, ResourceEntryT, ResourceEntryTT, };
-//# sourceMappingURL=index_type.d.ts.map
